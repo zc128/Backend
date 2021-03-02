2d17dbca7c84cf1bf290eb21496b0fa07b8a5091
ZCai <70999115+zc128@users.noreply.github.com>
2021-03-02 00:36:32 -0500

    Update with Users table

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings
-
+mobileApp.tables.add('Users');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
